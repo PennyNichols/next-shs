8fa4b3c89b1b3cecafb489d8a37ed73ef6a6fe05
penny.nichols <pnichols789@gmail.com>
2025-04-02 21:51:27 -0400

    import getDoc

--- hooks/FirebaseService.js
+++ hooks/FirebaseService.js
@@ -1,7 +1,7 @@
 import React, { createContext, useContext } from 'react';
 import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import { db } from '../firebase';
-import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc } from 'firebase/firestore';
+import { collection, addDoc, getDocs, doc, deleteDoc, updateDoc, getDoc } from 'firebase/firestore';
 
 const FirebaseCollectionContext = createContext();
 
